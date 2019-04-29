c17e04bc84b9fa8bed1e199b750a182918cb1add
Guillaume Englert <65494326+genglert@users.noreply.github.com>
2019-04-29 13:43:54 +0200

    refs #25 The label of the "submit" button for Opportunities creation form has been fixed.
    API breaking:
     - The erroneous attribute 'AbstractOpportunity.submit_label' has been removed.

--- creme/opportunities/models/opportunity.py
+++ creme/opportunities/models/opportunity.py
@@ -2,7 +2,7 @@
 
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2009-2018  Hybird
+#    Copyright (C) 2009-2019  Hybird
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -125,7 +125,7 @@ class AbstractOpportunity(CremeEntity):
     # function_fields = CremeEntity.function_fields.new(_TurnoverField())
 
     creation_label = _('Create an opportunity')
-    submit_label   = _('Save the opportunity')
+    save_label     = _('Save the opportunity')
 
     search_score = 100
 
