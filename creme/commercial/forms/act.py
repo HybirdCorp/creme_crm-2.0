c36247af09d647160e27ce79e230a2fb05e896d5
Guillaume Englert <65494326+genglert@users.noreply.github.com>
2019-04-30 17:45:58 +0200

    refs #109 #14 The EntityFilter of objectives could not be changed when the related filter was not editable. So not custom filters were excluded (which was stupid) ; only not visible filters (ie: private filters owned by other users) had to be excluded.

--- creme/commercial/forms/act.py
+++ creme/commercial/forms/act.py
@@ -2,7 +2,7 @@
 
 ################################################################################
 #    Creme is a free/open-source Customer Relationship Management software
-#    Copyright (C) 2009-2018  Hybird
+#    Copyright (C) 2009-2019  Hybird
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
@@ -26,6 +26,7 @@ from django.utils.translation import ugettext_lazy as _, ugettext
 from creme.creme_core.forms import CremeForm, CremeEntityForm, CremeModelForm
 from creme.creme_core.forms.fields import FilteredEntityTypeField
 from creme.creme_core.forms.widgets import Label
+from creme.creme_core.models import EntityFilter
 
 from .. import get_act_model, get_pattern_model
 from ..models import ActObjective, ActObjectivePatternComponent
@@ -61,7 +62,10 @@ class ObjectiveForm(CremeModelForm):
             fields = self.fields
             efilter = instance.filter
 
-            if efilter and not efilter.can_view(self.user)[0]:
+            # TODO: add a method EntityFilter.can_list(self.user) to avoid a query
+            # if efilter and not efilter.can_view(self.user)[0]:
+            if efilter and not EntityFilter.get_for_user(self.user, content_type=instance.ctype)\
+                                           .filter(id=efilter.id).exists():
                 fields['ec_label'] = CharField(label=fields['entity_counting'].label,
                                                required=False, widget=Label,
                                                initial=_('The filter cannot be changed because it is private.'),
