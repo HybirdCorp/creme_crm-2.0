51fa2646dc08df4b05170ae99d8a49712dc70ebf
Guillaume Englert <genglert@hybird.org>
2020-06-25 12:00:35 +0200

    In "About" page, retrieve information from GIT instead of HG.

--- creme/settings.py
+++ creme/settings.py
@@ -467,7 +467,9 @@ DEFAULT_PAGE_SIZE_IDX = 1  # Index (0-based, in PAGE_SIZES) of the default size 
 URLIZE_TARGET_BLANK = False
 
 # URL used in the GUI to indicate the repository address
-REPOSITORY = 'https://bitbucket.org/hybird/creme_crm-2.0/src/'
+# REPOSITORY = 'https://bitbucket.org/hybird/creme_crm-2.0/src/'
+REPOSITORY = 'https://github.com/HybirdCorp/creme_crm-2.0'
+SCM = 'git'  # Other possible values: 'hg'
 
 # GUI [END]#####################################################################
 
